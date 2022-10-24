4b33fac8c0d1e79120d37419fa15f3d8a83f6c85
tfobadar <b.rashdi@tfoco.com>
2022-10-24 22:15:56 +0300

    new commit

--- next.config.js
+++ next.config.js
@@ -29,5 +29,19 @@ module.exports = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "//js-eu1.hs-analytics.net/analytics/1666638300000/26169725.js",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
  
